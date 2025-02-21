165a7df8ee8e44622b81d42998b6f208409576c8
aftab-khan-dev <aftabahmadkhan.dev@gmail.com>
2025-02-21 14:42:49 +0500

    intial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-blue-500 text-white text-3xl font-bold">
+      Tailwind is Working!
+    </div>
+  );
+}
+
+export default App;
